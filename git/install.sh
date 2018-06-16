393ab95ec69c7c01652caf554389ff50097d8b31
S. David <2100425+s-daveb@users.noreply.github.com>
2018-06-16 14:47:01 -0400

    Fixing .gitrc install files

--- git/install.sh
+++ git/install.sh
@@ -1,16 +1,20 @@
-#!/bin/sh
+#!/usr/bin/env bash
+
 
 # Script working directory
 SWD="$(dirname $0)"
 
 cd $SWD
 REPODIR=$PWD
-
-cd $HOME
-
-
+cd $OLDPWD
 
 unlink "${HOME}/.gitconfig"
+cp -v "${REPODIR}/gitconfig" "/tmp/.tmp.gitconfig"
 
-ln -sv "${REPODIR}/gitconfig" "$HOME/.gitconfig"
+sed -i'' -e "s/^#\([[:blank:]]*\)name\(.*\)/\1name\2/g" /tmp/.tmp.gitconfig
 
+echo -n "Please to be entering your git display name> "
+read USER
+
+sed -i.bak  "s/AAAA BBBB/${USER}/g" /tmp/.tmp.gitconfig
+mv -v "/tmp/.tmp.gitconfig" "${HOME}/.gitconfig"
